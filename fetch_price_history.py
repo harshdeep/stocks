e0b0ba3c6c75b6da0f9863bf6ba5c50f5892e1cc
Harshdeep Singh <harshdeeps@fb.com>
2021-12-12 12:13:17 -0800

    Imported yfinance

--- fetch_price_history.py
+++ fetch_price_history.py
@@ -1,4 +1,13 @@
+import yfinance as yf
 from watchlist import watchlist
 
+class PriceHistoryFetcher:
+    def __init__(self) -> None:
+        pass
+
+    def fetch(self, symbols, start_date, end_date):
+        pass
+
 if __name__ == "__main__":
+    msft = yf.Ticker("MSFT")
     print(watchlist)
