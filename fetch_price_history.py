12274c1ccb7c9e65fcc915316ffc744b767b800b
Harshdeep Singh <harshdeeps@fb.com>
2021-12-12 09:41:28 -0800

    watchlist

---
+++ fetch_price_history.py
@@ -0,0 +1,4 @@
+from watchlist import watchlist
+
+if __name__ == "__main__":
+    print(watchlist)
---
+++ watchlist.py
@@ -0,0 +1,10 @@
+# TODO: Maybe move it to a db table
+
+watchlist = set([
+    'AAPL',
+    'ABNB',
+    'ADBE',
+    'AMZN',
+    'BA',
+    'BEAM',
+])
