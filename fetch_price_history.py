3a3651679585f1ee5a31a24db0b98638a1bbd9cb
Harshdeep Singh <harshdeeps@fb.com>
2021-12-14 21:48:14 -0800

    Fetching price history with yfinance, storing it in a csv, and fetching incrementally

--- fetch_price_history.py
+++ fetch_price_history.py
@@ -1,13 +1,67 @@
+from datetime import date, timedelta, datetime
+from pandas.core.frame import DataFrame
 import yfinance as yf
 from watchlist import watchlist
+import pandas as pd
+import numpy as numpy
 
 class PriceHistoryFetcher:
-    def __init__(self) -> None:
-        pass
+    FILE_NAME = "prices.csv"
+    def __init__(self, symbols) -> None:
+        self.symbols = symbols
+        self.symbolStr = " ".join(symbols)
 
-    def fetch(self, symbols, start_date, end_date):
-        pass
+    def download(self, start_date):
+        return yf.download(self.symbolStr, start=start_date)["Adj Close"]
+
+    def store(self, data: DataFrame) -> None:
+        data.to_csv(self.FILE_NAME)
+
+    def read_stored(self) -> DataFrame:
+        stored = pd.read_csv(self.FILE_NAME, index_col='Date')
+        stored.index = pd.to_datetime(stored.index)
+        return stored
+
+    def fetch_fresh(self):
+        data = self.download("2019-01-01")
+        data.to_csv(self.FILE_NAME)
+        return data
+
+    def last_date_with_data(self, data: DataFrame) -> date:
+        vti = data.loc[:, ['VTI']]
+        num_rows = vti.shape[0]
+        index = num_rows - 1
+        while index >= 0 and numpy.isnan(vti.iat[index, 0]):
+            index -= 1
+        return vti.index[index]
+
+    def fetch_incremental(self):
+        # fetch current data
+        stored = self.read_stored()
+
+        # if it's not outdated, just return it
+        last_date_with_data = self.last_date_with_data(stored).date()
+        if last_date_with_data >= date.today():
+            print("Not updating because we have today's data")
+            return stored
+
+        # else fetch the delta
+        start_date = last_date_with_data + timedelta(days = 1)
+        downloaded = self.download(start_date)
+
+        # insert delta
+        updated_rows = stored[stored.index.isin(downloaded.index)]
+        print("Dropping {0} rows".format(updated_rows.shape[0]))
+
+        stored.drop(updated_rows.index, inplace=True)
+
+        print("Appending {0} rows".format(downloaded.shape[0]))
+        updated = stored.append(downloaded)
+
+        # store updated data
+        self.store(updated)
+        return updated
 
 if __name__ == "__main__":
-    msft = yf.Ticker("MSFT")
-    print(watchlist)
+    phf = PriceHistoryFetcher(watchlist)
+    phf.fetch_incremental()
