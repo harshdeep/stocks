51ed87bf86430283c528a6c80d61166108bc275a
Harshdeep Singh <harshdeeps@fb.com>
2021-12-18 19:28:52 -0800

    Account for dates with missing data while fetching price history

--- price_history.py
+++ price_history.py
@@ -1,17 +1,19 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from fetch_price_history import PriceHistoryFetcher
 from watchlist import watchlist
+import numpy
 
 class PriceHistory:
     def __init__(self) -> None:
         phf = PriceHistoryFetcher(watchlist)
         self.prices = phf.fetch_incremental()
-        #self.prices = phf.fetch_fresh()
 
     def price(self, date, symbol):
+        while date not in self.prices.index or numpy.isnan(self.prices.loc[date, symbol]):
+            date = date - timedelta(days=1)
         return self.prices.loc[date, symbol]
 
 
 if __name__ == "__main__":
     p = PriceHistory()
-    print(p.price(datetime.fromisoformat('2021-12-13'), 'VTI'))
+    print(p.price(datetime.fromisoformat('2019-01-27'), 'AAPL'))
